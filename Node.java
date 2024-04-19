e9e7d87a966fd4e02ff3f7f2e99f9df2c272168f
xavier <vigenesisvi@gmail.com>
2024-04-19 09:30:11 -0600

    updated Node class

--- Node.java
+++ Node.java
@@ -1,3 +1,17 @@
-public class Node {
-  
+public class Node extends BT {
+  int num;
+  BT left;
+  BT right;
+
+  public Node(int num, BT left, BT right) {
+    this.num = num;
+    this.left = left;
+    this.right = right;
+  }
+
+  public int height() {
+    int leftHeight = left.height();
+    int rightHeight = right.height();
+    return (leftHeight < rightHeight ? rightHeight : leftHeight) + 1;
+  }
 }
