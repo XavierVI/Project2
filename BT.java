61a23cf1dc66a745dfa49a8105dd938b2b61c868
maisydunlavy <mdunlavy@unm.edu>
2024-04-28 13:41:56 -0600

    comments

--- BT.java
+++ BT.java
@@ -1,6 +1,11 @@
 abstract class BT {
   public abstract int height();
 
+  /**
+   * Builds a BT based on the given array
+   * @param arr the array to build the tree from
+   * @return root node of the tree
+   */
   public static Node buildTree(int [] arr){
     Node root = new Node(arr[0], Nil.getNil(), Nil.getNil());
     for(int i = 1; i < arr.length; i++){
@@ -9,6 +14,12 @@ abstract class BT {
     return root;
 
   }
+
+  /**
+   * Inserts a number into the tree by traversing to the correct leaf node position
+   * @param num the number to insert
+   * @param root the root of the tree
+   */
   public static void insert (int num, Node root){
     if(num < root.num){
       if(root.left == Nil.getNil()){
