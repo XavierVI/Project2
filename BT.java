3a6241dc8a2c7831fcdc98c1b22c88bb840d8336
maisydunlavy <mdunlavy@unm.edu>
2024-04-28 13:22:33 -0600

    BT insert

--- BT.java
+++ BT.java
@@ -1,3 +1,25 @@
 abstract class BT {
   public abstract int height();
+
+  public static Node buildTree(int [] arr){
+    Node root = null;
+    for(int i = 0; i < arr.length; i++){
+      root = insert(arr[i], root);
+    }
+    return root;
+
+
+  }
+  public static Node insert (int num, Node root){
+    if(root == null){
+      return new Node(num, null, null);
+    }
+    if(num < root.num){
+      root.left = insert(num, (Node) root.left);
+    }else if(num > root.num){
+      root.right = insert(num, (Node) root.right);
+    }
+    return root;
+   
+  }
 }
