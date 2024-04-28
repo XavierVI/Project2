1151941aaab74f9cc59563196e4cdb20a80225a9
Ryan Scherbarth <ry.scherbarth@gmail.com>
2024-04-28 13:43:53 -0600

    formatting

--- BT.java
+++ BT.java
@@ -1,27 +1,28 @@
 abstract class BT {
   public abstract int height();
 
-  public static Node buildTree(int [] arr){
+  public static Node buildTree(int[] arr) {
     Node root = null;
-    for(int i = 0; i < arr.length; i++){
+    for (int i = 0; i < arr.length; i++) {
       root = insert(arr[i], root);
     }
     return root;
 
-
   }
-  public static Node insert (int num, Node root){
-    if(root == null){
+
+  public static Node insert(int num, Node root) {
+    if (root == null) {
       return new Node(num, null, null);
     }
-    if(num < root.num){
+    if (num < root.num) {
       root.left = insert(num, (Node) root.left);
-    }else if(num > root.num){
+    } else if (num > root.num) {
       root.right = insert(num, (Node) root.right);
     }
     return root;
-   
+
   }
+
   public Boolean isBST(BT input, int min, int max) {
 
     if (input == null) {
@@ -38,7 +39,7 @@ abstract class BT {
 
       // Recurse
       return isBST(((Node) input).left, min, ((Node) input).num - 1) &&
-        isBST(((Node) input).right, ((Node) input).num + 1, max);
+          isBST(((Node) input).right, ((Node) input).num + 1, max);
     } else {
       // Handle casting to NIL error
       return true;
