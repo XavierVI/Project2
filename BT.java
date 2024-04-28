f3781959d468aee25f0077aa4ad1a5bc483a7e7f
Ryan Scherbarth <ry.scherbarth@gmail.com>
2024-04-28 13:40:29 -0600

    isBST method

--- BT.java
+++ BT.java
@@ -1,3 +1,27 @@
 abstract class BT {
   public abstract int height();
+
+  public Boolean isBST(BT input, int min, int max) {
+
+    if (input == null) {
+      return true;
+    }
+
+    if (input instanceof Node) {
+
+      int val = ((Node) input).num;
+
+      if (val < min || val > max) {
+        return false;
+      }
+
+      // Recurse
+      return isBST(((Node) input).left, min, ((Node) input).num - 1) &&
+        isBST(((Node) input).right, ((Node) input).num + 1, max);
+    } else {
+      // Handle casting to NIL error
+      return true;
+    }
+  }
+
 }
