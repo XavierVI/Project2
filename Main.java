c7cfc41bd02542f72562e7797958e29986b8e371
Ryan Scherbarth <ry.scherbarth@gmail.com>
2024-04-28 13:46:50 -0600

    more BST tests, looks it ain't workin 🥺

--- Main.java
+++ Main.java
@@ -23,9 +23,20 @@ public class Main {
   }
 
   private static void isBSTTest(){
+    // Test with valid BST
     BT nil = Nil.getNil();
     BT tree =  new Node(3, new Node(1, nil, new Node(4, nil, nil)), new Node(2, nil, nil));
-    System.out.print("\nIs BST test: ");
-    System.out.println(tree.isBST(tree, Integer.MIN_VALUE, Integer.MAX_VALUE) == false);
+    System.out.print("\nIs BST test with valid BST: ");
+    System.out.println(tree.isBST(tree, Integer.MIN_VALUE, Integer.MAX_VALUE) == true);
+
+    // Test with invalid BST
+    BT tree2 =  new Node(3, new Node(1, nil, new Node(4, nil, nil)), new Node(2, nil, nil));
+    System.out.print("\nIs BST with invalid BST Test: ");
+    System.out.println(tree2.isBST(tree, Integer.MIN_VALUE, Integer.MAX_VALUE) == false);
+
+    // test with empty tree 
+    BT emptyTree = Nil.getNil();
+    System.out.print("\nIs BST test with empty tree: ");
+    System.out.println(emptyTree.isBST(emptyTree, Integer.MIN_VALUE, Integer.MAX_VALUE) == true);
   }
 }
